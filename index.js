f013f0a12de39770417372cdacffd7e3d19440c1
deztopia <jason.melendez@gmail.com>
2015-07-07 17:14:44 -0700

    Add main script for npm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var globals = {};
 // Stash old global.
 if ("tree-node" in global) globals.tree-node = global.tree-node;
 
-module.exports = require("./dist/tree-node.js");
+module.exports = require("./dist/tree-node");
 
 // Restore old global.
 if ("tree-node" in globals) global.tree-node = globals.tree-node; else delete global.tree-node;
